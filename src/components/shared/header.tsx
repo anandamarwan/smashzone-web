5b2edc0fd63af34ce106cde7505c873721a28710
anandamarwan <anandapuspita2@gmail.com>
2024-07-16 15:29:14 +0700

    feat: improve nav

--- src/components/shared/header.tsx
+++ src/components/shared/header.tsx
@@ -6,9 +6,12 @@ export function Header() {
       <nav className="p-4 fixed w-full z-20 top-0 start-0 border-b">
         <div className="flex justify-between items-center max-w-7xl">
           <Link to={`/`} className="text-xl text-black">
-            <div className="flex flex-col">
-              <span>Smash</span>
-              <span>Zone 🏸</span>
+            <div className="flex gap-2 items-center">
+              <div className="flex flex-col">
+                <span>Smash</span>
+                <span>Zone </span>
+              </div>
+              <span className="text-4xl">🏸</span>
             </div>
           </Link>
           <ul className="flex flex-row gap-10  text-sm text-black ">
